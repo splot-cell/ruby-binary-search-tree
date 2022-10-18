ba6a800fb879e9cb2f34c54f74d0efdc63deb727
Olly Seber <olly_s@hotmail.co.uk>
2022-10-17 21:42:41 -0700

    Add #pretty_print method to Tree

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -4,11 +4,17 @@ class Tree
   attr_accessor :root
 
   def initialize(arr)
-    root = build_tree(arr.sort)
+    root = build_tree(arr.uniq.sort)
   end
 
   def build_tree(arr)
 
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
 
