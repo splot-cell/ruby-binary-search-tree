1425c3acdbfa246bb0141f3f3035f2c135e4c971
Olly Seber <olly_s@hotmail.co.uk>
2022-10-17 21:41:25 -0700

    Initialize Tree class

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+class Tree
+  attr_accessor :root
+
+  def initialize(arr)
+    root = build_tree(arr.sort)
+  end
+
+  def build_tree(arr)
+
+  end
+end
+
 class Node
   include Comparable
   attr_accessor :data, :left_child, :right_child
